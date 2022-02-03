7de3ece7b6b062f727e24a85acc115bb8f69f5ed
steelemeg <71856754+steelemeg@users.noreply.github.com>
2022-02-02 20:08:44 -0600

    Update smallsh.c

--- smallsh.c
+++ smallsh.c
@@ -46,11 +46,13 @@ int main(int argc, char* argv[]) {
     // TODO get rid of this--go three times for starters
     int keepGoing = 3;
     while (keepGoing > 0) {
+        // Mimicing the formatting of the screenshots
+        printStartTerminal();
         // Get the user's input and build a command struct from it
         char* input = getRawInput();
         struct command* newCommand = createCommand(input);
 
-        printStartTerminal();
+        
         // If the input isn't blank, execute the instruction 
         if (newCommand->instruction) {
             // TODO Step 1 : let's just print things ok
