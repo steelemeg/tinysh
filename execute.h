5541d30ea2e31ba90ad759af338e25c651afb41b
steelemeg <71856754+steelemeg@users.noreply.github.com>
2022-02-07 14:45:13 -0600

    Update execute.h

--- execute.h
+++ execute.h
@@ -80,7 +80,7 @@ void execCd(struct command* currCommand) {
 void execStatus() {
     char* output = calloc(20, sizeof(char));
     exploreValues();
-    if (lastFGTerminate) { sprintf(output, "exit value %d", lastFGExitStatus); }
+    if (!lastFGTerminate) { sprintf(output, "exit value %d", lastFGExitStatus); }
     else { sprintf(output, "terminated by signal %d", lastFGExitStatus); }
     printShout(output, true);
     free(output);
