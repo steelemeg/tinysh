aa28e4ed32358c152277535139994d804d4211fb
steelemeg <71856754+steelemeg@users.noreply.github.com>
2022-02-06 22:31:27 -0600

    Update smallsh.c

--- smallsh.c
+++ smallsh.c
@@ -80,6 +80,7 @@ int main(int argc, char* argv[]) {
         char* protoCommand = getExpandedInput();
         // Build a command struct from the expanded input.
         struct command* newCommand = createCommand(protoCommand);
+        displayCommand(newCommand);
         free(protoCommand);
         // If the input wasn't blank, execute the instruction 
         if (newCommand->instruction) {
