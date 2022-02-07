59feeb8446742ac2850853c37f337a76ef58ff1f
steelemeg <71856754+steelemeg@users.noreply.github.com>
2022-02-07 14:32:59 -0600

    why

--- execute.h
+++ execute.h
@@ -153,6 +153,7 @@ void execLibrary(struct command* currCommand) {
         else { 
             if (debugMessages) { printShout("Foreground job setup begins", true); }
             childCreatedInBackground = false;
+            printShout("SETTING THE DANG THING", true);
             observeSIGINT(true);
         }
 
